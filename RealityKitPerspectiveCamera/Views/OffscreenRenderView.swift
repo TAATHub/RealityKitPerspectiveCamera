b7244c4f04d7769e2be46359e7d6597089881a9f
TAATHub <20purpleblue10@gmail.com>
2025-02-08 15:22:48 +0900

    Render to LowLevelTexture directly from RealityRenderer

--- RealityKitPerspectiveCamera/Views/OffscreenRenderView.swift
+++ RealityKitPerspectiveCamera/Views/OffscreenRenderView.swift
@@ -7,14 +7,6 @@ struct OffscreenRenderView: View {
     @State private var model: OffscreenRenderModel = .init()
     @State private var tasks: Set<Task<Void, Never>> = []
     
-    @State private var texture: LowLevelTexture?
-    private let commandQueue: MTLCommandQueue
-    
-    init() {
-        let device = MTLCreateSystemDefaultDevice()!
-        self.commandQueue = device.makeCommandQueue()!
-    }
-    
     private var timer: AsyncStream<Void> {
         AsyncStream {
             try? await Task.sleep(for: .seconds(1/30))
@@ -22,9 +14,16 @@ struct OffscreenRenderView: View {
     }
     
     var body: some View {
-        RealityView { content in
-            let entity = try! renderEntity()
-            content.add(entity)
+        ZStack {
+            if model.isRendererUpdated {
+                RealityView { content in
+                    if let entity = try? renderEntity() {
+                        content.add(entity)
+                    }
+                }
+            } else {
+                Color.black
+            }
         }
         .onChange(of: appModel.immersiveSpaceState, { _, state in
             switch state {
@@ -49,9 +48,6 @@ struct OffscreenRenderView: View {
                                 guard let transform = appModel.cameraTransform else { return }
                                 try? model.render(position: transform.translation,
                                                   orientation: transform.rotation * simd_quatf(angle: -.pi, axis: .init(x: 0, y: 1, z: 0)))
-                                if let texture = model.renderTexture {
-                                    updateTexture(with: texture)
-                                }
                             }
                         }
                     }
@@ -64,57 +60,24 @@ struct OffscreenRenderView: View {
     }
 }
 
+// ref: https://gist.github.com/Matt54/c4d2ce778ffb6f9d2ddc6b8c7332c7d5
 extension OffscreenRenderView {
-    private var lowLevelTextureDescriptor: LowLevelTexture.Descriptor {
-        var desc = LowLevelTexture.Descriptor()
-
-        desc.textureType = .type2D
-        desc.arrayLength = 1
-
-        desc.width = 1600
-        desc.height = 900
-        desc.depth = 1
-
-        desc.mipmapLevelCount = 1
-        desc.pixelFormat = .bgra8Unorm
-        desc.textureUsage = [.shaderRead, .shaderWrite]
-        desc.swizzle = .init(red: .red, green: .green, blue: .blue, alpha: .alpha)
-
-        return desc
-    }
-    
-    private func renderEntity() throws -> Entity {
-        let mesh = MeshResource.generatePlane(width: 1.6, height: 0.9)
-        let texture = try LowLevelTexture(descriptor: lowLevelTextureDescriptor)
-        let resource = try TextureResource(from: texture)
+    private func renderEntity() throws -> Entity? {
+        guard let lowLevelTexture = model.lowLevelTexture,
+              let resource = try? TextureResource(from: lowLevelTexture) else { return nil }
         
         var material = UnlitMaterial()
         material.color.texture = .init(resource)
-        material.opacityThreshold = 0.5
         
+        let mesh = MeshResource.generatePlane(width: 1.6, height: 0.9)
         let modelComponent = ModelComponent(mesh: mesh, materials: [material])
+        
         let entity = Entity()
         entity.components.set(modelComponent)
         entity.scale *= 0.3
         entity.position = [0, 0, -0.1]
 
-        self.texture = texture
         return entity
-    }
-    
-    private func updateTexture(with mtlTexture: MTLTexture) {
-        guard let texture else { return }
-        guard let commandBuffer = commandQueue.makeCommandBuffer(),
-              let blitEncoder = commandBuffer.makeBlitCommandEncoder()
-        else { return }
-        
-        commandBuffer.enqueue()
-        
-        let outTexture: MTLTexture = texture.replace(using: commandBuffer)
-        blitEncoder.copy(from: mtlTexture, to: outTexture)
-        
-        blitEncoder.endEncoding()
-        commandBuffer.commit()
     }
 }
 
